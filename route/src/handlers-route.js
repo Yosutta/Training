38c7f6ae1c085a598a3429159aba2491f4b568ce
Yosutta <phuphuongtin@gmail.com>
2022-02-27 18:32:27 +0700

    Reformat files

--- route/src/handlers-route.js
+++ route/src/handlers-route.js
@@ -1,63 +1,63 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
 // Example1
 app.get(
-  "/request",
+  '/request',
   (req, res, next) => {
-    console.log("This is the first call back function");
-    next(); // Hand off control to the next function
+    console.log('This is the first call back function')
+    next() // Hand off control to the next function
   },
   (req, res) => {
-    res.send("This is the request");
+    res.send('This is the request')
   }
-);
+)
 
 // Example2
 const checkBoolean1 = (req, res, next) => {
-  const a = 10;
+  const a = 10
   if (a > 5) {
-    console.log("TRUE");
+    console.log('TRUE')
   }
-  next();
-};
+  next()
+}
 
 const checkBoolean2 = (req, res, next) => {
-  const b = 1;
+  const b = 1
   if (!b < 5) {
-    console.log("FALSE");
+    console.log('FALSE')
   }
-  next();
-};
+  next()
+}
 
-app.get("/boolean", [checkBoolean1, checkBoolean2], (req, res) => {
-  res.send("Comparing value!");
-});
+app.get('/boolean', [checkBoolean1, checkBoolean2], (req, res) => {
+  res.send('Comparing value!')
+})
 
 // Example3
 const firstCallBack = (req, res, next) => {
-  console.log("This is the first callback");
-  next();
-};
+  console.log('This is the first callback')
+  next()
+}
 
 const secondCallBack = (req, res, next) => {
-  console.log("This is the second callback");
-  next();
-};
+  console.log('This is the second callback')
+  next()
+}
 
 app.get(
-  "/callback",
+  '/callback',
   [firstCallBack, secondCallBack],
   (req, res, next) => {
-    console.log("This is the third callback");
-    next();
+    console.log('This is the third callback')
+    next()
   },
   (req, res) => {
-    res.send("This is the final callback");
+    res.send('This is the final callback')
   }
-);
+)
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+  console.log('Listening on port 3000')
+})
--- route/src/methods-route.js
+++ route/src/methods-route.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Kyanon");
-});
+app.get('/', (req, res) => {
+  res.send('Welcome to Kyanon')
+})
 
-app.get("/get", (req, res) => {
-  res.send(`This is a get request's response`);
-});
+app.get('/get', (req, res) => {
+  res.send(`This is a get request's response`)
+})
 
-app.post("/post", (req, res) => {
-  res.send(`This is a post request's response`);
-});
+app.post('/post', (req, res) => {
+  res.send(`This is a post request's response`)
+})
 
-app.all("/secret", (req, res, next) => {
-  console.log("Requesting a secret message from the server");
-  next();
-});
+app.all('/secret', (req, res, next) => {
+  console.log('Requesting a secret message from the server')
+  next()
+})
 
-app.get("/secret", (req, res) => {
-  res.send("The secret is ....");
-});
+app.get('/secret', (req, res) => {
+  res.send('The secret is ....')
+})
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+  console.log('Listening on port 3000')
+})
